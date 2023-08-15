dc295b6fd12c4493e328f7c4f30731249aae071d
aolufisayo <phissycodes@hotmail.com>
2023-08-15 14:43:00 +0100

    implemented crud endpoints for comment resource

--- src/main/java/com/phissy/blog/controller/CommentController.java
+++ src/main/java/com/phissy/blog/controller/CommentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,6 +22,27 @@ public class CommentController {
     public ResponseEntity<Comment> addComment(@PathVariable("postId") UUID postId ,@Valid @RequestBody Comment comment){
         return new ResponseEntity<>(commentService.addComment(postId,comment), HttpStatus.CREATED);
     }
+
+    @GetMapping("{postId}/comments")
+    public ResponseEntity<List<Comment>> getAllComments(@PathVariable("postId") UUID postId){
+        return new ResponseEntity<>(commentService.getAllCommentByPostId(postId), HttpStatus.OK);
+    }
+
+    @GetMapping("{postId}/comments/{commentId}")
+    public ResponseEntity<Comment> getCommentByPostIdAndCommentId(@PathVariable("postId") UUID postId, @PathVariable("commentId") UUID commentId){
+        return new ResponseEntity<>(commentService.getCommentByPostIdAndId(postId, commentId), HttpStatus.OK);
+    }
+
+    @PutMapping("{postId}/comments/{commentId}")
+    public ResponseEntity<Comment> updateComment(@PathVariable("postId") UUID postId, @PathVariable("commentId") UUID commentId,@RequestBody Comment newComment){
+        return new ResponseEntity<>(commentService.updateComment(postId, commentId, newComment), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{postId}/comments/{commentId}")
+    public void deleteComment(@PathVariable("postId") UUID postId, @PathVariable("commentId") UUID commentId){
+        commentService.deleteComment(postId, commentId);
+    }
+
 
 
 }
